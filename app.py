8eefff2f476626dfcc931e4dd86aef5f8288d7ba
Jon <jdittert@DESKTOP-573LJ3S.localdomain>
2022-11-01 11:34:20 -0400

    Copy framework from finance

--- app.py
+++ app.py
@@ -9,3 +9,21 @@ from werkzeug.security import check_password_hash, generate_password_hash
 # Configure application
 app = Flask(__name__)
 
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///seatingchart.db")
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
